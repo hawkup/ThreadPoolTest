6237e4edd61a081ccceb801d60651d7c45924e93
hawkup <spiritsquare@gmail.com>
2021-10-30 20:42:21 +0700

    add thread pool

--- src/com/example/threadpooltest/ThreadPoolTest.java
+++ src/com/example/threadpooltest/ThreadPoolTest.java
@@ -1,7 +1,21 @@
 package com.example.threadpooltest;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class ThreadPoolTest {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(5);
+
+
+        for (int i = 0; i < 100; i++) {
+            long start = System.currentTimeMillis();
+            executor.execute(() -> {
+                System.out.println("Hello World");
+                long end = System.currentTimeMillis();
+                System.out.println("start: " + start + " end: " + end + " takes: " +
+                        (end - start) + "ms");
+            });
+        }
     }
 }
